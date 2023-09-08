ac7f92a3a747e51ae1b8a0d960102e0df541c159
RaltsHunter <117216708+RaltsHunter@users.noreply.github.com>
2023-09-08 12:59:03 -0500

    Started pacman code

---
+++ 625Project1/Assets/scripts/PacmanScript.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PacmanScript : MonoBehaviour
+{
+    public float mSpeed = 50;
+    public float rSpeed;
+    public Rigidbody rb;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float vAxis = Input.GetAxis("Vertical");
+        float hAxis = Input.GetAxis("Horizontal");
+        gameObject.transform.Translate(gameObject.transform.forward * Time.deltaTime * mSpeed, Space.World);
+        gameObject.transform.Rotate(0, rSpeed * Time.deltaTime * hAxis, 0);
+    }
+}
